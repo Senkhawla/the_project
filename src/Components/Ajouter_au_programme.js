7ce3e7e03ff14657bc02d9fe07c827f5fb14b4f9
khawla <senkadikhawla@gmail.com>
2023-05-03 15:50:06 +0100

    RELIAGE : AJOUTER AU PROGRAMME

--- src/Components/Ajouter_au_programme.js
+++ src/Components/Ajouter_au_programme.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import './Ajouter_Modifier_programme.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+
+
 function Ajouter_au_programme() {
+  const [chapter, setChapter] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [coverage, setCoverage] = useState('');
+  const [needed_proofs, setNeededProofs] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      chapter,
+      title,
+      description,
+      coverage,
+      needed_proofs
+    };
+
+    axios.post('https://server-social-benefits.vercel.app/addSocialBenefit', data)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+
     return (
       <div>
       <div className="sidebar">
@@ -9,11 +40,11 @@ function Ajouter_au_programme() {
             </div>
             <h1 className='title'>Ajouter au programme :</h1> 
             <div className='outside'>
-            <div className='big-container'>
+            <form className='big-container' onSubmit={handleSubmit}>
            
 
-            <label htmlFor="service">Service:</label>
-            <select type="text" className='inpu'   placeholder="--Selectionner un service"  name="name"  >
+            <label htmlFor="service" >Service:</label>
+            <select type="text" className='inpu'   placeholder="--Selectionner un service"  name="name" onChange={(event) => setChapter(event.target.value)}  >
             <option value=""> Select Service</option>
               <option value="Opérations Chirurgicales">Opérations Chirurgicales</option>
               <option value="Bains Minéraux">Bains Minéraux</option>
@@ -23,10 +54,10 @@ function Ajouter_au_programme() {
               <option value="Bourse pour nouveau-né">Bourse pour nouveau-né</option>
             </select> 
            <label htmlFor="title">Title:</label>
-            <input type="text" className='input'  placeholder="Title"  />
+            <input type="text" className='input'  placeholder="Title" onChange={(event) => setTitle(event.target.value)} />
             <div className='input-description'> 
             <label htmlFor="Desciption">Desciption:</label>
-            <textarea type="text" className='inputinput'  placeholder="Desciption"  />
+            <textarea type="text" className='inputinput'  placeholder="Desciption" onChange={(event) => setDescription(event.target.value)} />
             </div> 
             <label htmlFor="montant">Mantant/Pourcentage de Remboursement:</label>
             <div className='input-montantpoursentage'> 
@@ -34,7 +65,7 @@ function Ajouter_au_programme() {
             <h1 className='Mantant-fixe'>Montant fixe</h1>
              <div className="montantpourcentage">
              
-            <input type="text" className='input'  placeholder="Montant"  />
+            <input type="text" className='input'  placeholder="Montant" onChange={(event) => setCoverage(event.target.value)} />
               </div>
               <input className='input-radio' type="radio" id="option3" name="options" value="option3"/>
               <h2 className='pourcentage'>Pourcentage</h2>
@@ -42,16 +73,16 @@ function Ajouter_au_programme() {
               <input type="text" className='input'  placeholder="Pourcentage" /> 
              </div>
              <label htmlFor="montant">Papiers Demandées:</label>
-             <textarea type="text" className='inputinput'  placeholder="Papiers Demandées" />
+             <textarea type="text" className='inputinput'  placeholder="Papiers Demandées" onChange={(event) => setNeededProofs(event.target.value)}/>
               </div>
 
              <div class="btn-ajouter-au-programme"> 
-                 <button type='button'>Ajouter au Programme</button>
+                 <button type='submit'>Ajouter au Programme</button>
              </div> 
   
   
 
-            </div> 
+            </form> 
             
              </div>   
  </div>
